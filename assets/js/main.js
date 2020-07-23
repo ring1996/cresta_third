5ca7004f01de9bb1296c844332630f0488c7e850
Miu <miu.taji@gmail.com>
2020-07-23 13:43:34 +0900

    FIX: スクロールでヘッターの見た目変更JS修正

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,21 +20,62 @@ jQuery(function ($) {
 });
 
 /*==================================================
+** SP Menu
+==================================================*/
+jQuery(function ($) {
+  var gnav = $('#js-nav'),
+    menuBtn = $('#js-btn'),
+    body = $(document.body),
+    classToAssignGnav = 'is-active-gnav';
+
+  function toggleGnav() {
+    if(body.hasClass(classToAssignGnav)) {
+      gnav.fadeOut(200);
+      body.removeClass(classToAssignGnav);
+    } else {
+      gnav.fadeIn();
+      body.addClass(classToAssignGnav);
+    }
+  }
+  menuBtn.on('click', toggleGnav);
+});
+
+/*==================================================
 ** headerChange
 ==================================================*/
-const mvHeight = window.innerHeight;
-const scrollHeight = window.pageYOffset;
-const body = document.querySelector('body');
-const classToAssignHeader = 'is-active-header';
+(function() {
+  let mvHeight;
+  const body = document.querySelector('body');
+  const classToAssignHeader = 'is-active-header';
 
-function headerChange() {
-  if (mvHeight <= scrollHeight) {
-    body.classList.add(classToAssignHeader);
-  } else {
-    body.classList.remove(classToAssignHeader);
+  function fetchMvHeight() {
+    return mvHeight = window.innerHeight;
   }
-}
 
-window.addEventListener('scroll', headerChange);
-window.addEventListener('load', headerChange);
+  function headerChange() {
+    let scrollHeight = window.pageYOffset;
+    console.log('----------');
+    console.log(mvHeight);
+    console.log(scrollHeight);
+
+    if (mvHeight <= scrollHeight) {
+      body.classList.add(classToAssignHeader);
+    } else {
+      body.classList.remove(classToAssignHeader);
+    }
+  }
+
+  window.addEventListener('load', fetchMvHeight);
+  window.addEventListener('resize', fetchMvHeight);
+  window.addEventListener('scroll', headerChange);
+  window.addEventListener('load', headerChange);
+})();
+
+
+/*==================================================
+** SP Menu
+==================================================*/
+(function() {
+  const body = document.querySelector('body');
+})();
 
